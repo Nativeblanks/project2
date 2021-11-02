c6710655e3497e7b8cd285901ef6c87dee64a4dc
PaulThomasWI <87770976+PaulThomasWI@users.noreply.github.com>
2021-11-02 17:05:49 -0500

    uploading schema and seeds

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,16 +4,31 @@ DROP DATABASE IF EXISTS employees;
 -- CREATE DATABASE
 CREATE DATABASE employees;
 
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS Address;
-
 USE employees;
 
-CREATE TABLE employee (
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS address;
+
+CREATE TABLE address 
+(
+  Address_id INTEGER NOT NULL AUTO_INCREMENT,
+  Organization varchar(64) NOT NULL, 
+  AddressLine1 VARCHAR(45) NOT NULL,
+  AddressLine2 VARCHAR(45) NULL,
+  City VARCHAR(45) NOT NULL,
+  State VARCHAR(2) NOT NULL,
+  Zip VARCHAR(10) NOT NULL,
+  Phone VARCHAR(45) NULL,
+  Fax VARCHAR(45) NULL,
+  PRIMARY KEY (Address_id)
+);
+
+CREATE TABLE employee 
+(
   employee_id INT NOT NULL AUTO_INCREMENT,
   Firstname VARCHAR(45) NOT NULL,
   Lastname VARCHAR(45) NOT NULL,
-  Title VARCHAR(45) NOT NULL,
+  Title VARCHAR(100) NOT NULL,
   Phone VARCHAR(45) NULL,
   Cell VARCHAR(45) NULL,
   Email VARCHAR(128) NOT NULL,
@@ -23,16 +38,6 @@ CREATE TABLE employee (
   FacebookAccount VARCHAR(45) NULL,
   TwitterAccount VARCHAR(45) NULL,
   Address_id INT NOT NULL,
-  PRIMARY KEY (employee_id)
-  FOREIGN KEY (Address_id) REFERENCES Address(Address_id));
-
-  CREATE TABLE Address (
-  Address_id INTEGER NOT NULL AUTO_INCREMENT,
-  AddressLine1 VARCHAR(45) NOT NULL,
-  AddressLine2 VARCHAR(45) NULL,
-  City VARCHAR(45) NOT NULL,
-  State VARCHAR(2) NOT NULL,
-  Zip VARCHAR(10) NOT NULL,
-  Phone VARCHAR(45) NULL,
-  Fax VARCHAR(45) NULL,
-  PRIMARY KEY (Address_id));
+  PRIMARY KEY (employee_id),
+  FOREIGN KEY (Address_id) REFERENCES address(Address_id)
+);
