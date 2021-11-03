1e427c651af90c9c241d7be8693f039778cf9a0a
Zach B <nativeblanks@gmail.com>
2021-11-03 19:12:06 -0400

    push

--- Develop/models/employee.js
+++ Develop/models/employee.js
@@ -10,11 +10,11 @@ Employee.init(
     , Phone: {type: DataTypes.STRING, allowNull: true}
     , Cell: {type: DataTypes.STRING, allowNull: true}
     , Email: {type: DataTypes.STRING, allowNull: false}
-    , Hiredate: {type: DataTypes.DATETIME, allowNull: false}
-    , Dateofbirth: {type: DataTypes.DATETIME, allowNull: false}
+    , Hiredate: {type: DataTypes.DATE, allowNull: false}
+    , Dateofbirth: {type: DataTypes.DATE, allowNull: false}
     , Linkedin: {type: DataTypes.STRING, allowNull: true}
-    , FacebookAccount: {type: DataTypes.STRING, allowNull: true}
-    , TwitterAccount: {type: DataTypes.STRING, allowNull: true}
+    , Facebookaccount: {type: DataTypes.STRING, allowNull: true}
+    , Twitteraccount: {type: DataTypes.STRING, allowNull: true}
     , Address_id: {type: DataTypes.INTEGER, allowNull: false}
   }
   ,
