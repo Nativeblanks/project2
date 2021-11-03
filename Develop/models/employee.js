98f48b357735cf5deebdcdd3fd391eb37fda986b
Prime <jeff.d.937@gmail.com>
2021-11-02 20:57:39 -0400

    'model'

---
+++ Develop/models/employee.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+class Employee extends Model {}
+Employee.init(
+  {
+      employee_id: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true}
+    , Firstname: {type: DataTypes.STRING, allowNull: false}
+    , Lastname: {type: DataTypes.STRING, allowNull: false}
+    , Title: {type: DataTypes.STRING, allowNull: false}
+    , Phone: {type: DataTypes.STRING, allowNull: true}
+    , Cell: {type: DataTypes.STRING, allowNull: true}
+    , Email: {type: DataTypes.STRING, allowNull: false}
+    , Hiredate: {type: DataTypes.DATETIME, allowNull: false}
+    , Dateofbirth: {type: DataTypes.DATETIME, allowNull: false}
+    , Linkedin: {type: DataTypes.STRING, allowNull: true}
+    , FacebookAccount: {type: DataTypes.STRING, allowNull: true}
+    , TwitterAccount: {type: DataTypes.STRING, allowNull: true}
+    , Address_id: {type: DataTypes.INTEGER, allowNull: false}
+  }
+  ,
+  {
+    sequelize
+    , timestamps: false
+    , freezeTableName: true
+    , underscored: true
+    , modelName: 'employee'
+  }
+);
+module.exports = Employee;
---
+++ Develop/models/index.js
@@ -0,0 +1,62 @@
+// import all models
+//const Post = require('./Post');
+//const User = require('./User');
+const Employee = require('./Employee');
+const Address = require('./Address');
+
+// create associations
+Address.hasMany(Employee, {
+  foreignKey: 'address_id'
+});
+
+Employee.belongsTo(Address, {
+  foreignKey: 'address_id'
+});
+/*
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+*/
+
+module.exports = { Employee, Address };
+
