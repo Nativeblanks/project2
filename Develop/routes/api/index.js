65be6cd84672d1df99f5288e0897246473b4fbf5
Zach B <nativeblanks@gmail.com>
2021-11-08 15:35:22 -0500

    merge from main for updated code

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,19 +1,8 @@
 const router = require("express").Router();
 const employeeRoutes = require("./employee-routes");
 const addressRoutes = require("./address-routes");
-<<<<<<< HEAD
-const homeRoutes = require('api/home-routes.js');
-
-//const tagRoutes = require('./tag-routes');
 
 router.use("/employee", employeeRoutes);
 router.use("/address", addressRoutes);
-
-//router.use('/tags', tagRoutes);
-=======
-
-router.use("/employee", employeeRoutes);
-router.use("/address", addressRoutes);
->>>>>>> 5d9e6e30a6e0f34686134f5f3780177914578f88
 
 module.exports = router;
