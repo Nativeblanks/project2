a4f5134cb786b1d7afd540421a047cc88623c476
Conner Rhodes <connerrhodes.25@gmail.com>
2021-11-08 21:50:38 -0500

    Login

--- Develop/routes/home-routes.js
+++ Develop/routes/home-routes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
+const sequelize = require('./../config/connection');
+const { Employee, User, Address } = require('../models');
 
 router.get('/', (req, res) => {
+  console.log(req.session)
   res.render('homepage');
 });
 
-router
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+
 
 module.exports = router;
