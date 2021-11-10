7f40fcc29999e4725f379283f9a20dea12d23109
Your Name <shelbyfayemo@gmail.com>
2021-11-09 23:07:14 -0500

    addform

--- Develop/public/javascript/view-all.js
+++ Develop/public/javascript/view-all.js
@@ -1,12 +1,20 @@
-function viewall (event) {
-    event.preventDefault();
-    console.log("view all button pressed")
-    const response = fetch(`/api/employee`) , {
-        method: 'PUT',
-        body:
-
-    }
-    
-
+function viewall(event) {
+  event.preventDefault();
+  console.log("view all button pressed");
+  const response = await fetch("/api/employees", {
+    method: "post",
+    body: JSON.stringify({
+      Firstname,
+      Lastname,
+      Title,
+    }),
+    headers: { "Content-type": "application/json" },
+  });
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+  console.log("view all button pressed");
 }
-document.querySelector('#view-all').addEventListener('click', viewall);
+document.querySelector("#view-all").addEventListener("click", viewall);
