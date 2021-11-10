21f25135658cd91cb0b0f545f16e8edd87cf8ed2
PaulThomasWI <87770976+PaulThomasWI@users.noreply.github.com>
2021-11-09 20:29:15 -0600

    pushing up my code

--- Develop/routes/api/user-routes.js
+++ Develop/routes/api/user-routes.js
@@ -45,16 +45,22 @@ else{
   });
 });
 
-  router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    }
-    else {
-      res.status(404).end();
-    }
-  });
+/*
+  ======================================================================
+    Logout route
+  ======================================================================
+*/
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+  
   router.get("/", (req, res) => {
     User.findAll({})
       .then((dbData) => res.json(dbData))
